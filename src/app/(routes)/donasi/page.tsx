3fad6ef3c12e32d3008235095af7c9ddc54de628
Marcelino <marcelinokenny12@gmail.com>
2025-01-15 15:17:34 +0700

    Create donation page

---
+++ src/app/(routes)/donasi/page.tsx
@@ -0,0 +1,296 @@
+import Image from 'next/image';
+
+export default function Donasi(){
+  return(
+    <div className="bg-slate-50 bg-[url('/pattern.png')] bg-repeat bg-contain">
+      <section className="relative bg-[#15575B]  h-40 lg:h-32 mt-40 border-y-4 border-black flex justify-center">
+        <div className="bg-[#F4AA3D] border-4 rounded-[50px] lg:rounded-[70px] border-black h-44 lg:h-40 top-[105px] lg:-top-4 w-[85%] lg:w-[65%] absolute flex flex-col items-center justify-center">
+          <h2 className="font-heading text-xl lg:text-4xl text-white font-bold z-20 mb-4">Hadirkan senyuman,</h2>
+          <h2 className="font-heading text-xl lg:text-4xl text-white font-bold z-20 max-w-48 text-center lg:max-w-full">wujudkan cinta di bulan suci Ramadan</h2>
+        </div>
+        <Image
+            src="/maskot.webp"
+            alt="Maskot"
+            width={300}
+            height={300}
+            className="size-44 -mr-10 lg:mr-0 top-28 lg:size-72 lg:-top-16 absolute right-0 z-0 overflow-hidden"
+          />
+      </section>
+
+      <section className="relative flex min-h-[220px] bg-[#15575B] mt-24 border-y-4 border-black ">
+        <div className="flex items-center justify-center w-[45%]">
+          <h2 className='font-title text-xl text-white max-w-[300px] italic leading-relaxed'>"Ramadhan Penuh <span className='font-bold'>Berkah</span>, Mari Raih <span className='font-bold'>Pahala Jariyah</span>  Mulai dari <span className='font-bold'>Satu Rupiah</span>"</h2>
+        </div>
+        <div className="relative w-[55%]">
+          <div className="flex flex-col justify-center p-12 absolute inset-0 bg-[#F4AA3D] border-[7px] border-y-0 border-black rounded-l-[100px]">
+            <h2 className='font-body text-3xl text-[#15575B] font-semibold mb-2'>Titipkan Kebaikan,</h2>
+            <h2 className='font-heading text-4xl text-white font-bold'>Tebarkan <br/>Keberkahan</h2>
+          </div>
+        </div>
+        <Image
+            src="/speaker.png"
+            alt="Maskot"
+            width={300}
+            height={300}
+            className="size-44 -mr-10 lg:left-[500px] lg:mr-0 top-28 lg:size-56 lg:top-20 absolute z-0 overflow-hidden"
+          />
+      </section>
+
+
+      <section className='flex font-body items-center lg:min-h-32 border-black border-4 mt-36 mx-3 md:mx-72 rounded-[70px] p-10 md:text-xl lg:leading-loose lg:tracking-widest italic'>
+        <h2>"Kamu sekali-kali tidak akan memperoleh kebajikan (yang sempurna) sebelum kamu menginfakkan sebagian harta yang kamu cintai. Apa pun yang kamu infakkan, sesungguhnya Allah Maha Mengetahui tentangnya." (QS. Ali Imran : 92)</h2>
+      </section>
+
+      <section className='flex mx-52 gap-x-4 mt-10'>
+          <div className='font-body w-[82%] p-10 text-white text-xl tracking-wider italic leading-loose font-medium min-h-32 bg-[#F4AA3D] rounded-[35px] border-4 border-black'>
+            <h2>Donasi Langsung melalui COD Sekitar Kampus UGM / Sekretariat Jama’ah Shalahuddin (Sayap Selatan Masjid Kampus UGM)</h2>
+          </div>
+          <div className='relative w-[18%] min-h-32 bg-[#15575B] rounded-[35px] border-4 border-black'>
+          <Image
+            src="/koin.png"
+            alt="Maskot"
+            width={300}
+            height={300}
+            className='size-36 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+            />
+          </div>
+      </section>
+
+
+
+
+      <section className='flex relative mx-10'>
+        <div className='w-full relative'>
+          <Image
+                src="/maskot.webp"
+                alt="Maskot"
+                width={500}
+                height={500}
+                className="size-44 scale-x-[-1] -mr-10 lg:mr-0 lg:size-[400px] lg:-bottom-10 absolute left-0 z-0 overflow-hidden"
+          />
+        </div>
+        <section className='flex flex-col justify-center min-h-[500px] mt-20'>
+            <section className='flex mb-20 mx-28'>
+                <div className='flex  items-center justify-center min-h-20 bg-[#F4AA3D] rounded-tl-[30px] w-1/4 border-4 border-r-0 border-black'>
+                  <Image
+                    src="/paperclip.png"
+                    alt="Maskot"
+                    width={60}
+                    height={60}
+                    className='p-2'
+                  />
+                </div>
+                <div className="flex items-center justify-center min-h-20 bg-[#15575B] rounded-br-[30px] w-3/4 border-4 border-black border-l-0">
+                  <h1 className='font-heading text-5xl text-white p-4'>Jenis Donasi</h1>
+                </div>
+            </section>
+            <section className='flex gap-28'>
+              <div className="relative w-2/6">
+                <div className="absolute -top-4 -left-4 w-full h-24 bg-[#CCB087] border-4 border-black"></div>
+                <div className="relative bg-[#9F4230] text-white px-4 flex items-center justify-between border-4 border-black w-60 h-24">
+                  <span className="font-heading font-bold text-3xl">Uang</span>
+                  <Image
+                    src="/moneybag.png"
+                    alt="Money Bag"
+                    width={300}
+                    height={300}
+                    className="size-12"
+                  />
+                </div>
+              </div>
+
+              {/* Logistik Card */}
+              <div className="relative w-2/6">
+                <div className="absolute -top-4 -right-4 w-full h-24 bg-[#CCB087] border-4 border-black"></div>
+                <div className="relative bg-[#F4AA3D] text-white px-4  flex items-center justify-between border-4 border-black w-60 h-24">
+                <span className="font-heading font-bold text-3xl">Logistik</span>
+                  <Image
+                    src="/logistik.png"
+                    alt="Logistik"
+                    width={300}
+                    height={300}
+                    className="size-10"
+                  />
+                </div>
+              </div>
+
+              <div className="relative w-2/6">
+                <div className="absolute -top-4 -right-4 w-full h-full bg-[#76D0D5] border-4 border-black"></div>
+                <div className="relative bg-[#42A0A6] text-white px-4  flex items-center justify-between border-4 border-black w-60 h-24">
+                  <span className="font-heading font-bold text-3xl">Makanan</span>
+                      <Image
+                        src="/makanan.png"
+                        alt="Logistik"
+                        width={300}
+                        height={300}
+                        className="size-10"
+                      />
+                </div>
+              </div>
+            </section>
+            <section className='flex mt-20 gap-16 items-center justify-center'>
+              <div className='relative min-h-20 w-full bg-[#15575B] border-[3px] border-black ml-10'>
+                <div className='absolute flex items-center justify-center bg-white min-h-10 rounded-b-full w-20 -top-4 left-4 border-2 border-black'>
+                  <Image
+                    src="/ic_cp.svg"
+                    alt="CP Logo"
+                    width={32}
+                    height={32}
+                    className="size-7"
+                  />
+                </div>
+                <div className='flex flex-col text-right'>
+                  <h2 className='font-body italic px-4 py-2 font-semibold text-white '>Contact Person</h2>
+                  <hr className='border-black border-[1.5px] w-[80%] ml-auto'/>
+                  <h2 className='font-body italic px-4 py-2 font-semibold text-white'> 081377660189 (Rofi’ Khoirullah)</h2>
+                </div>
+              </div>
+              <div className='relative min-h-20 w-full bg-[#CCB087] border-[3px] border-black mr-10'>
+                <div className='absolute flex items-center justify-center bg-white min-h-10 rounded-b-full w-20 -top-4 right-4 border-2 border-black'>
+                  <Image
+                    src="/ic_cp2.svg"
+                    alt="CP Logo"
+                    width={32}
+                    height={32}
+                    className="size-6"
+                  />
+                </div>
+                <h2 className='font-body italic px-4 py-2 font-semibold '>Contact Person</h2>
+                <hr className='border-black border-[1.5px] w-[80%]'/>
+                <h2 className='font-body italic px-4 py-2 font-semibold'> 081377660189 (Rofi’ Khoirullah)</h2>
+               
+              </div>
+            </section>
+        </section>
+      </section>
+
+
+
+      <section className='flex items-center justify-center xl:mx-96 mt-20'>
+          <div className='flex items-center justify-center bg-[#F4AA3D] p-12 w-full mt-20 rounded-full border-4 border-black'>
+            <p className='text-5xl italic font-body '>Donasi Iftar dan Sahur</p>
+          </div>
+      </section>
+
+      <section className='flex flex-col items-center justify-center min-h-32 border-4 rounded-[40px]  border-black  mx-4 xl:mx-[400px] mt-10'>
+        <h2 className='text-center text-xl font-body text-[#15575B] italic p-4 max-w-4xl leading-normal'>
+          Seluruh donasi digunakan untuk 42.000 porsi buka bersama dan 10.000 porsi sahur selama acara 
+          Ramadhan Di Kampus UGM 1446 H
+        </h2>
+      </section>
+
+
+      <section className="relative flex min-h-24 mb-10  bg-white border-black border-4 rounded-full mt-20 xl:mx-96 ">
+        <div className="flex items-center bg-[#15575B] border-r-4 border-black rounded-full justify-center w-1/2">
+          <h2 className='font-medium font-body text-2xl text-white italic '>Update Donasi</h2>
+        </div>
+        <div className="relative w-1/2">
+          <div className="flex justify-center items-center p-8 absolute inset-0 bg-white border-y-0 rounded-r-full border-black">
+            <h2 className='font-semibold font-body text-2xl text-[#15575B] italic'>Rp 1.000.000.000.000,-</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className='flex flex-col xl:mx-[470px] xl:h-[600px] border-black border-4'>
+          <div className='flex flex-col items-center justify-center h-3/4 bg-[#15575B]'>
+              <Image
+                src="/qrislogo.png"
+                alt="QRIS Logo"
+                width={250}
+                height={250}
+              />
+              <Image
+                src="/qrismaskam.png"
+                alt="QRIS Maskam"
+                width={250}
+                height={250}
+              />
+              <h2 className='text-xl font-body italic text-white mt-4'>a.n Masjid Kampus UGM</h2>
+          </div>
+          <div className='flex bg-[#F4AA3D] h-1/4 items-center justify-evenly'>
+              <div className='flex flex-col'>
+              <Image
+                src="/ic_bsi.png"
+                alt="BSI Logo"
+                width={150}
+                height={150}
+              />
+              <Image
+                src="/ic_muamalat.png"
+                alt="Bank Muamalat Logo"
+                width={150}
+                height={150}
+              />
+              </div>
+              <div className='flex flex-col'>
+                  <div className='font-body text-lg text-white mt-4'>
+                    <h2 className='-mb-1'>7191219494</h2>
+                    <h2 className='text-sm'>a.n Masjid Kampus UGM</h2>
+                  </div>
+                  <div className='font-body text-lg text-white'>
+                    <h2 className='-mb-1'>5350007800</h2>
+                    <h2 className='text-sm'>a.n Masjid Kampus UGM</h2>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+
+      <section className='flex items-center justify-center xl:mx-96 mt-20'>
+          <div className='flex items-center justify-center bg-[#15575B] p-12 w-full mt-20 rounded-full border-4 border-black'>
+            <p className='text-5xl italic font-body text-white '>Donasi Kegiatan dan Kajian</p>
+          </div>
+      </section>
+
+      <section className='flex flex-col items-center justify-center min-h-32 border-4 rounded-[40px]  border-black  mx-4 xl:mx-[400px] mt-10'>
+        <h2 className='text-center text-xl font-body text-[#15575B] italic p-10 max-w-xl leading-normal'>
+        Seluruh donasi digunakan untuk  rangkaian acara Ramadhan Di Kampus UGM 1446 H
+        </h2>
+      </section>
+
+      <section className="relative flex min-h-24 mb-10  bg-white border-black border-4 rounded-full mt-20 xl:mx-96 ">
+        <div className="flex items-center bg-[#F4AA3D] border-r-4 border-black rounded-full justify-center w-1/2">
+          <h2 className='font-medium font-body text-2xl text-white italic '>Update Donasi</h2>
+        </div>
+        <div className="relative w-1/2">
+          <div className="flex justify-center items-center p-8 absolute inset-0 bg-white border-y-0 rounded-r-full border-black">
+            <h2 className='font-semibold font-body text-2xl text-[#15575B] italic'>Rp 1.000.000.000.000,-</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className='flex flex-col xl:mx-[470px] xl:h-[600px] border-black border-4'>
+          <div className='flex flex-col items-center justify-center h-3/4 bg-[#15575B]'>
+            <Image
+                src="/qrislogo.png"
+                alt="QRIS Logo"
+                width={250}
+                height={250}
+              />
+              <Image
+                src="/qrismaskam.png"
+                alt="QRIS Maskam"
+                width={250}
+                height={250}
+              />
+              <h2 className='text-xl font-body italic text-white mt-4'>a.n Masjid Kampus UGM</h2>
+          </div>
+          <div className='flex bg-[#F4AA3D] h-1/4 items-center justify-evenly'>
+              <Image
+                src="/ic_bsi.png"
+                alt="BSI Logo"
+                width={150}
+                height={150}
+              />
+              <div className='font-body text-lg text-white'>
+                  <h2 className='-mb-1'>5350007800</h2>
+                  <h2 className='text-sm'>a.n Masjid Kampus UGM</h2>
+              </div>
+          </div>
+      </section>
+
+    </div>
+  )
+}
+
+  
