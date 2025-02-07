0a5ec9832da4aec4b8d687a6c6ebf2f64245e448
Djatiaja <djamgt23@gmail.com>
2025-02-07 09:16:00 +0700

    FAQ done

--- src/app/components/faq/Faq.tsx
+++ src/app/components/faq/Faq.tsx
@@ -1,20 +1,68 @@
 'use client'
 import React, { useState } from 'react';
 import { FAQHolder } from './FaqHolder';
-import { faqData } from './Data';
+import { FaqData, faqData } from './Data';
 import FaqTitle from './FaqTitle';
 
 export default function FAQ() {
+
+    const fetchFaqData = async () => {
+        // Simulate an API call
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(faqData);
+            }, 1000);
+        });
+    };
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    React.useEffect(() => {
+        const getData = async () => {
+            const response = await fetch('http://localhost:1337/api/faqs');
+            const result = await response.json();
+            setData(result.data);
+            setLoading(false);
+        };
+        getData();
+    }, []);
 
     return (
         <section className="py-28">
             <FaqTitle />
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-                <div className="accordion-group" data-accordion="default-accordion">
-                    {faqData.map((item, index) => (
-                        <FAQHolder key={index} answer={item.answer} question={item.question} />
-                    ))}
-                </div>
+                {loading ? (
+                    <div className="flex justify-center items-center">
+                        <div className="loader"></div>
+                    <div className="throbber"></div>
+                    <style jsx>{`
+                        .throbber {
+                            border: 4px solid rgba(0, 0, 0, 0.1);
+                            width: 36px;
+                            height: 36px;
+                            border-radius: 50%;
+                            border-left-color: #15575B;
+                            animation: spin 1s ease infinite;
+                        }
+
+                        @keyframes spin {
+                            0% {
+                                transform: rotate(0deg);
+                            }
+                            100% {
+                                transform: rotate(360deg);
+                            }
+                        }
+                    `}</style>
+                    </div>
+                ) : (
+                    <div className="accordion-group" data-accordion="default-accordion">
+                        {data.map((item:FaqData, index) => (
+                            <FAQHolder key={index} answer={item.answer} question={item.question} />
+                        ))}
+                    </div>
+                )}
             </div>
         </section>
     );
--- src/app/components/faq/FaqTitle.tsx
+++ src/app/components/faq/FaqTitle.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 export default function FaqTitle() {
     return (
         <>
-            <div className="bg-greenCS lg:h-32 justify-center items-center flex lg:my-20">
+            <div className="bg-greenCS lg:h-32 justify-center items-center flex lg:my-20 lg:py-6 md:py-1 md:my-12 md:-mt-4 -mt-2 my-8">
                 <Image
                         src="/images/maskot.png"
                         alt="Logo"
-                        width={294}
-                        height={294}
-                        className="absolute right-0 lg:mr-20 transform -scale-x-100 lg:-mt-20"
+                        width={230}
+                        height={230}
+                        className="absolute lg:mr-20 -scale-x-100 lg:-mt-12 md:w-[200px] md:h-[200px] lg:w-[230px] lg:h-[230px] md:-mt-8 right-0 sm:w-[130px] sm:h-[130px] -mt-4 w-[100px] h-[100px] -mt-4"
                     />
                 <div>
-                    <h1 className=" font-bold text-white p-4 text-center text-4xl font-titania ">Hello Sobat RDK!
+                    <h1 className="font-bold text-white lg:p-4 md:p-4 p-2 text-center lg:text-4xl font-titania md:text-3xl sm:text-lg ">Hello Sobat RDK!
                     <br/> Is there anything we can help with?</h1>
                 </div >
 
