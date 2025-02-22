efd036d0127889810357064a6f8d168a7d7bf9bc
wahhab <wahhabawaludin2004@mail.ugm.ac.id>
2025-02-22 15:03:57 +0700

    FOOTER FIX

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
 			<div className="flex grid grid-cols-2 items-center text-white w-full bg-greenCS border-black md:border-y-[3px] lg:border-y-4 border-y-2 relative">
 				{/* Ramadhan penuh berkah */}
 				<div className="italic text-center lg:p-10 lg:text-3xl md:text-xl font-light text-xs 2xl:text-end 2xl:mr-10 font-title font-thin">
-					<p >
+					<p>
 						“Ramadan Penuh <span className="font-black">Berkah,</span>
 					</p>
 					<p>
@@ -42,13 +42,17 @@ export default function Footer() {
 				/>
 
 				{/* Titipkan Kebaikan */}
-				<div className="text-white bg-orangeCS border-black rounded-s-full md:border-l-[3px]  border-l-2 md:p-10 p-3 pl-10  md:flex md:justify-center ">
-					<div>
+				<div className="text-white bg-orangeCS border-black rounded-s-full md:border-l-[3px]  border-l-2 md:p-10 p-3 pl-10  md:flex md:justify-center 2xl:justify-start  ">
+					<div className="2xl:ml-10">
 						<p className="lg:text-3xl md:text-xl text-greenCS text-xs font-bold">
 							Titipkan Kebaikan,
 						</p>
-						<p className="lg:text-5xl md:text-2xl text-sm font-heading">Tebarkan</p>
-						<p className="lg:text-5xl text-sm md:text-2xl font-heading">Keberkahan</p>
+						<p className="lg:text-5xl md:text-2xl text-sm font-heading">
+							Tebarkan
+						</p>
+						<p className="lg:text-5xl text-sm md:text-2xl font-heading">
+							Keberkahan
+						</p>
 					</div>
 
 					<div />
@@ -56,9 +60,9 @@ export default function Footer() {
 			</div>
 
 			{/* Section 2 */}
-			<div className="md:flex lg:mt-5 justify-center xl:justify-start w-fit lg:w-full mx-auto xl:max-w-7xl ">
+			<div className="md:flex lg:mt-5 justify-center items-center xl:justify-start w-fit lg:w-full mx-auto xl:max-w-7xl ">
 				{/* Kiri */}
-				<div className="w-11/12 md:w-[44%]  lg:w-[47%] mx-auto italic text-xs mt-10 md:mt-0 md:p-1 lg:p-0 xl:max-w-xl">
+				<div className="mx-auto p-2 md:w-[44%] lg:w-[47%] italic text-xs mt-10 md:mt-0 md:p-1 lg:p-0 xl:max-w-xl">
 					<div className="flex justify-center items-center">
 						<Image
 							src="/logo_with_name.png"
@@ -76,17 +80,16 @@ export default function Footer() {
 						/>
 					</div>
 
-					<div className="lg:w-2/3 mx-auto text-center text-greenCS font-semibold mt-4 ">
-						<p>Donasi Langsung melalui COD Sekitar Kampus UGM /</p>
-
-						<p>Sekretariat Jama’ah Shalahuddin (Sayap Selatan Masjid</p>
-
-						<p>Kampus UGM)</p>
+					<div className="lg:w-2/3 mx-auto text-center text-greenCS font-semibold mt-4 text-[10px] md:text-base ">
+						<p>
+							Donasi Langsung melalui COD Sekitar Kampus UGM / Sekretariat
+							Jama’ah Shalahuddin (Sayap Selatan Masjid Kampus UGM)
+						</p>
 					</div>
 
 					{/* Jenis Donasi */}
 					<div>
-						<p className="bg-greenCS text-white mt-4 py-1 px-20 rounded-full w-fit mx-auto">
+						<p className="bg-greenCS text-white mt-4 py-1 px-20 rounded-full w-fit mx-auto text-xs md:text-sm lg:text-base">
 							Jenis Donasi
 						</p>
 
@@ -125,11 +128,11 @@ export default function Footer() {
 
 					{/* Narahubung Donasi */}
 					<div>
-						<div className="flex justify-evenly items-center text-greenCS mt-5 text-xs gap-2">
-							<p className="bg-greenCS text-white  rounded-xl  text-center p-2 w-[180px] xl:w-full">
+						<div className="flex justify-evenly items-center text-greenCS mt-5 md:text-xs lg:text-sm gap-2 text-[10px]">
+							<p className="bg-greenCS text-white  rounded-xl  text-center p-2 w-[140px] md:w-[180px] xl:w-full">
 								Narahubung Donasi Kegiatan
 							</p>
-							<p className="bg-greenCS text-white rounded-xl p-2 w-[180px] xl:w-full text-center">
+							<p className="bg-greenCS text-white rounded-xl p-2 w-[140px] md:w-[180px] xl:w-full text-center">
 								Narahubung Donasi Iftar dan Sahur
 							</p>
 						</div>
@@ -137,17 +140,19 @@ export default function Footer() {
 						<div className="flex justify-evenly items-center text-greenCS font-bold mt-6 text-sm gap-2 text-center ">
 							{/* Narahubung Donasi Kegiatan */}
 							<Link href="https://wa.me/62895332785320" target="_blank">
-								<p className="rounded-2xl border-2 md:border-[3px] lg:border-4 border-[#15575B] py-2 px-4 md:px-0 lg:px-3 md:text-xs lg:text-sm">
-									0895332785320 (Salma Arsepti)
-								</p>
+								<div className="rounded-2xl border-2 md:border-[3px] lg:border-4 border-[#15575B] py-2 px-4 md:px-5 lg:px-6 text-xs md:text-sm lg:text-base">
+									<p>0895332785320</p>
+									<p>(Salma Arsepti)</p>
+								</div>
 							</Link>
 
 							{/* Narahubung Donasi Buka & Sahur */}
 
 							<Link target="_blank" href="https://wa.me/6281377660189">
-								<p className="rounded-2xl border-2 md:border-[3px] lg:border-4 border-[#15575B] py-2 px-4 md:px-0 lg:px-3 md:text-xs  lg:text-sm ">
-									081377660189 (Rofi’ Khoirullah)
-								</p>
+								<div className="rounded-2xl border-2 md:border-[3px] lg:border-4 border-[#15575B] py-2 px-4 md:px-5 lg:px-6 text-xs md:text-sm lg:text-base">
+									<p>081377660189</p>
+									<p>(Rofi’ Khoirullah)</p>
+								</div>
 							</Link>
 						</div>
 					</div>
@@ -155,9 +160,9 @@ export default function Footer() {
 					{/* Konfirmasi Donasi */}
 					<div className="flex justify-between ">
 						{/* Konfirmasi Donasi Operasional */}
-						<div className="mt-6 w-fit mx-auto rounded-lg border-2 lg:border-4 border-[#15575B] transform -translate-x-2 md:translate-x-0 ">
-							<div className="text-xs bg-greenCS text-white p-1 text-center md:py-1 lg:p-1 ">
-								<p className="w-[120px] lg:w-full mx-auto">
+						<div className="mt-6 w-fit mx-auto rounded-lg border-2 lg:border-4 border-[#15575B]">
+							<div className="text-[10px] md:text-xs bg-greenCS text-white p-1 text-center md:py-1 lg:p-1 ">
+								<p className="w-[120px] lg:w-full mx-auto text-[10px] md:text-xs lg:text-sm">
 									Konfirmasi Donasi Kegiatan
 								</p>
 							</div>
@@ -165,11 +170,11 @@ export default function Footer() {
 								href="https://bit.ly/DonasiOperasionalRDK46H"
 								target="_blank"
 							>
-								<p className="text-greenCS font-semibold p-2 lg:text-base xl:hidden text-center">
+								<p className="text-greenCS font-semibold p-2 lg:text-base xl:hidden text-center text-[10px] md:text-xs">
 									bit.ly/DonasiOperasional RDK46H
 								</p>
 
-								<p className="text-greenCS font-semibold p-2 lg:text-base hidden xl:block">
+								<p className="text-greenCS font-semibold p-2 lg:text-base hidden xl:block text-[10px] md:text-base">
 									bit.ly/DonasiOperasionalRDK46H
 								</p>
 							</Link>
@@ -178,12 +183,12 @@ export default function Footer() {
 						{/* Konfirmasi Donasi sahur Buka */}
 						<div className="mt-6 w-fit mx-auto rounded-lg border-2 lg:border-4 border-[#15575B] ml-2 md:ml-4">
 							<div className="text-xs bg-greenCS text-white p-1 text-center md:py-1 lg:p-1 ">
-								<p className="w-[120px] lg:w-full mx-auto">
+								<p className="w-[120px] lg:w-full mx-auto text-[10px] md:text-xs lg:text-sm">
 									Konfirmasi Donasi Sahur & Buka
 								</p>
 							</div>
 							<Link href="https://bit.ly/DonasiSahurBukaRDK46H" target="_blank">
-								<p className="text-greenCS font-semibold p-2 lg:text-base xl:hidden text-center">
+								<p className="text-greenCS font-semibold p-2 lg:text-base xl:hidden text-center text-[10px] md:text-xs">
 									bit.ly/DonasiSahurBuka RDK46H
 								</p>
 
@@ -198,7 +203,7 @@ export default function Footer() {
 				{/* Right */}
 				<div className="grid grid-cols-1 md:grid-cols-2  border-black md:w-1/2 lg:w-[47%] mx-auto md:border-[3px] lg:border-4 my-5 mt-10 lg:mt-5 w-11/12 xl:max-w-xl h-fit">
 					{/* Donasi Iftar dan Sahur */}
-					<div className="border-2 md:border-0 border-black lg:mb-0 bg-green-400">
+					<div className="border-2 md:border-0 border-black lg:mb-0">
 						{/* Title */}
 						<div className="text-center bg-orangeCS lg:text-xl py-2 lg:border-b-4 border-b-2 border-black font-heading">
 							<p>Donasi</p>
@@ -270,7 +275,7 @@ export default function Footer() {
 					</div>
 
 					{/* Donasi Kegiatan */}
-					<div className="border-2 border-black lg:mb-0 md:border-l-[3px] lg:border-l-4 md:border-0 bg-green-400 mt-10 md:mt-0">
+					<div className="border-2 border-black lg:mb-0 md:border-l-[3px] lg:border-l-4 md:border-0 mt-10 md:mt-0 ">
 						{/* Title */}
 						<div className="text-center bg-orangeCS  lg:text-xl py-[1.37rem] md:py-[1.235rem] lg:py-[1.37rem] md:h-fit border-black  lg:border-b-4 border-b-2  md:border-b-[3px] lg:border-0 font-heading">
 							<p>Donasi Kegiatan</p>
@@ -342,7 +347,7 @@ export default function Footer() {
 					alt="Logo"
 					width={200}
 					height={200}
-					className="absolute top-[-0.5rem] left-[10%] lg:top-[-2rem] lg:left-[-1%] xl:left-[5rem] hidden md:block lg:w-[200px] md:w-[100px] "
+					className="absolute top-[-0.5rem] left-[10%] lg:top-[-2rem] lg:left-[-1%] xl:left-[8%] 2xl:left-[16%]  hidden md:block lg:w-[200px] md:w-[100px] "
 				/>
 
 				{/* Maskot Mobile*/}
@@ -357,9 +362,11 @@ export default function Footer() {
 
 			{/* Footer Bawah */}
 			<div className="bg-greenCS mt-10 text-white text-xs lg:text-base relative">
-				<div className="lg:py-7 lg:px-14 p-4 lg:flex  max-w-7xl mx-auto ">
+		
+				{/* Ini di Atas */}
+				<div className="lg:py-7 lg:px-14 p-4 lg:flex max-w-7xl mx-auto z-50 relative ">
 					{/* Kiri */}
-					<div className="">
+					<div >
 						<p className="font-heading lg:text-3xl text-base ">
 							Ramadhan Di Kampus
 						</p>
@@ -384,7 +391,11 @@ export default function Footer() {
 							/>
 
 							<div className="ml-2">
-								<Link href="https://g.co/kgs/h3kBLMV">
+								<Link
+									href="https://g.co/kgs/h3kBLMV"
+									className="hover:text-[#F4AA3D]"
+									target="_blank"
+								>
 									<p>
 										Sekretariat Jama’ah Shalahuddin, Sayap Selatan Masjid Kampus
 										UGM
@@ -405,7 +416,7 @@ export default function Footer() {
 							/>
 
 							<Link href="mailto:rdk@ugm.ac.id">
-								<p className="ml-2">rdk@ugm.ac.id</p>
+								<p className="ml-2 hover:text-[#F4AA3D]">rdk@ugm.ac.id</p>
 							</Link>
 						</div>
 
@@ -495,17 +506,15 @@ export default function Footer() {
 								<h3 className="font-heading">Kegiatan</h3>
 								<ul className="leading-7">
 									<li className="hover:text-[#F4AA3D] mt-3">
-										<Link href="/kegiatan/daily-agenda/#samudra">
-											Samudra
+										<Link href="/kegiatan/daily-agenda/#samudra">Samudra</Link>
+									</li>
+									<li className="hover:text-[#F4AA3D]">
+										<Link href="/kegiatan/daily-agenda/#mimbar-subuh">
+											Mimbar Subuh
 										</Link>
 									</li>
 									<li className="hover:text-[#F4AA3D]">
-										<Link href="/kegiatan/daily-agenda/#mimbar-subuh">Mimbar Subuh</Link>
-									</li>
-									<li className="hover:text-[#F4AA3D]">
-										<Link href="/kegiatan/daily-agenda/#rpl">
-											RPL
-										</Link>
+										<Link href="/kegiatan/daily-agenda/#rpl">RPL</Link>
 									</li>
 									<li className="hover:text-[#F4AA3D]">
 										<Link href="/kegiatan/bigagenda">Big Events</Link>
@@ -532,42 +541,50 @@ export default function Footer() {
 							{/* Ramadan di kampus */}
 							<div className="flex justify-center">
 								<Link href="https://www.instagram.com/rdkugm/" target="_blank">
-									<Image
-										src="/svg/instagram.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform  duration-1000 hover:scale-125 hover:rotate-180 hover:shadow-xl `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/instagram.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<Link href="https://x.com/rdkugm/" target="_blank">
-									<Image
-										src="/svg/x.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/x.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<Link href="https://www.tiktok.com/@rdkugm_" target="_blank">
-									<Image
-										src="/svg/tiktok.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/tiktok.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 								<Link href="https://www.youtube.com/@rdkugm" target="_blank">
-									<Image
-										src="/svg/youtube.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/youtube.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<p>Ramadhan Di Kampus UGM</p>
@@ -579,73 +596,85 @@ export default function Footer() {
 									href="https://www.instagram.com/masjidkampusugm/"
 									target="_blank"
 								>
-									<Image
-										src="/svg/instagram.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/instagram.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<Link href="https://x.com/masjidkampusugm/" target="_blank">
-									<Image
-										src="/svg/x.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/x.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<Link
 									href="https://www.tiktok.com/@masjidkampusugm"
 									target="_blank"
 								>
-									<Image
-										src="/svg/tiktok.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/tiktok.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<Link
 									href="https://www.youtube.com/@masjidkampusugm"
 									target="_blank"
 								>
-									<Image
-										src="/svg/youtube.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/youtube.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 								<Link
 									href="https://www.facebook.com/maskamugm/"
 									target="_blank"
 								>
-									<Image
-										src="/svg/facebook.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/facebook.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 								<Link
 									href="https://open.spotify.com/show/4HBxMmTZXQ3GQjBgw33yjw"
 									target="_blank"
 								>
-									<Image
-										src="/svg/spotify.svg"
-										alt="logo"
-										width={24}
-										height={24}
-										className={`object-cover mr-2 transition-transform duration-1000 hover:scale-125 hover:rotate-180 `}
-									/>
+									<div className="group">
+										<Image
+											src="/svg/spotify.svg"
+											alt="logo"
+											width={24}
+											height={24}
+											className="object-cover mr-2 transition-transform duration-1000 group-hover:rotate-[360deg] group-hover:scale-125 group-hover:shadow-xl"
+										/>
+									</div>
 								</Link>
 
 								<p>Masjid Kampus UGM</p>
@@ -654,13 +683,14 @@ export default function Footer() {
 					</div>
 				</div>
 
+				{/* Di bawah */}
+				<div className="absolute top-10 left-[-5rem] w-[25rem] h-[25rem] bg-[#76D0D5] rounded-full blur-[10rem] opacity-70 z-20"></div>
+
 				<div className="flex justify-center items-center p-6  bg-gradient-to-r from-[#22979E] from-[-100%] to-[#155458] ">
 					<p className="italic text-lg">
 						&copy; RDK UGM 1446 H. All Rights Reserved
 					</p>
 				</div>
-
-				<div className="absolute top-10 left-[-5rem] w-[25rem] h-[25rem] bg-[#76D0D5] rounded-full blur-[10rem] opacity-70"></div>
 			</div>
 		</div>
 	);
