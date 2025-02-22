c9801bf5398f5ca79017695b0539cbbe40188ba0
wahhab <wahhabawaludin2004@mail.ugm.ac.id>
2025-02-22 17:45:40 +0700

    FIX ARTICLE

--- src/app/(routes)/artikel/page.tsx
+++ src/app/(routes)/artikel/page.tsx
@@ -1,28 +1,14 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import HeroSlider from "./HeroSlider";
 import BigAgendaRamadan from "./BigAgendaRamadan";
 import Samudra from "./Samudra";
 import MimbarSubuh from "./MimbarSubuh";
-import Rpl from "./Rpl";
 import RamadanPublicLecture from "./Rpl";
-import Loading from "./loading";
 
 export default function Artikel() {
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			// Simulasi delay (bisa diganti dengan fetch API asli)
-			await new Promise((resolve) => setTimeout(resolve, 4000));
-			setIsLoading(false);
-		};
-
-		fetchData();
-	}, []);
 
 	return (
 		<section>
--- src/app/(routes)/artikel/samudra/HotArticle.tsx
+++ src/app/(routes)/artikel/samudra/HotArticle.tsx
@@ -1,18 +1,27 @@
-"use client";
-
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Autoplay, Navigation } from "swiper/modules";
-import { useEffect, useState } from "react";
-import { Article, fetchArticles } from "@/app/lib/article";
+import { fetchArticles, Article } from "@/app/lib/article";
+import Link from "next/link";
 
 export default function HotArticle() {
 	const [articles, setArticles] = useState<Article[]>([]);
 	const [loading, setLoading] = useState(true);
-	const [activeIndex, setActiveIndex] = useState(0);
+	const hotArticles = articles.filter(
+		(slide) =>
+			slide.category === "Samudera" &&
+			slide.like >= 50 &&
+			slide.like <= 100
+	);
+
+	// Jika Hot Articles kosong, menggunakan semua artikel dengan kategori "Samudera"
+	const displayedArticles =
+		hotArticles.length > 0
+			? hotArticles
+			: articles.filter((slide) => slide.category === "Samudera");
 
 	useEffect(() => {
 		const getArticles = async () => {
@@ -21,22 +30,22 @@ export default function HotArticle() {
 			setLoading(false);
 		};
 		getArticles();
-	}, []);
+	});
 
 	return (
-		<li>
+		<li className="mt-6 text-white max-w-7xl mx-auto">
 			{loading ? (
 				<div className="flex space-x-4 py-6">
-					{Array.from({ length: 3 }).map((_, index) => (
+					{Array.from({ length: 2 }).map((_, index) => (
 						<div
 							key={index}
-							className="bg-gray-200 animate-pulse w-[90%] lg:w-[25rem] h-[15rem] rounded-3xl"
+							className="bg-gray-200 animate-pulse w-[90%] lg:w-[1000px] lg:h-[500px] rounded-3xl"
 						></div>
 					))}
 				</div>
-			) : (
+			) : displayedArticles.length > 0 ? (
 				<Swiper
-					spaceBetween={20}
+					spaceBetween={10}
 					navigation={{
 						nextEl: ".custom-swiper-button-next",
 						prevEl: ".custom-swiper-button-prev",
@@ -49,65 +58,74 @@ export default function HotArticle() {
 					modules={[Navigation, Autoplay]}
 					slidesPerView={1}
 					breakpoints={{
-						640: { slidesPerView: 1, spaceBetween: 20 },
-						768: { slidesPerView: 2, spaceBetween: 20 },
-						1024: { slidesPerView: 3, spaceBetween: 0 },
+						640: {
+							slidesPerView: 1,
+						},
+						768: {
+							slidesPerView: 2,
+						},
 					}}
-					onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
 				>
-					{articles.length > 0 ? (
-						articles
-							.filter((slide) => slide.category === "Samudera")
-							.map((slide, index) => {
-								const totalBigAgenda = articles.filter(
-									(slide) => slide.category === "Samudera"
-								).length;
-								const isHighlighted =
-									index === (activeIndex + 2) % totalBigAgenda;
+					{displayedArticles.map((article) => (
+						<SwiperSlide key={article.id}>
+							<Link href={`/artikel/${article.id}`}>
+								<div className="relative transition-transform duration-300 ease-in-out hover:scale-105 max-w-2xl mx-auto">
+									{/* Gambar utama */}
+									<Image
+										alt="BAR"
+										src={
+											article.article_images?.[0]?.publicUrl || "/default.jpg"
+										}
+										width={800}
+										height={400}
+										className="lg:h-[400px] h-[300px] object-cover w-[600px] xl:w-[800px] "
+									/>
 
-								return (
-									<SwiperSlide key={slide.id}>
-										<div
-											className={`bg-white lg:shadow-lg border rounded-3xl my-4 mt-6 transition-transform duration-300 ease-in-out hover:scale-105 h-[15rem] ${
-												isHighlighted
-													? "z-10 lg:w-[25rem] lg:-translate-x-6"
-													: "lg:w-[90%]"
-											}`}
-										>
-											<Link href={`/artikel/${slide.id}`} passHref>
-												<Image
-													alt={slide.title}
-													src={
-														slide.article_images?.[0]?.publicUrl ||
-														"/default.jpg"
-													}
-													width={500}
-													height={300}
-													className="rounded-t-3xl bg-black h-40 object-cover"
-												/>
-												<div className="flex mx-4">
-													<Image
-														alt="like"
-														src="/svg/like.svg"
-														width={48}
-														height={48}
-														className="transition-transform scale-x-100 hover:scale-110 transition-transform duration-300 ease-in-out"
-													/>
-													<h3 className=" text-center font-bold italic text-greenCS text-sm md:text-xs lg:text-sm p-2 md:p-3 lg:p-2">
-														{slide.title}
-													</h3>
-												</div>
-											</Link>
-										</div>
-									</SwiperSlide>
-								);
-							})
-					) : (
-						<div className="text-center py-4 text-gray-500">
-							Tidak ada kajian
-						</div>
-					)}
+									{/* Informasi RDK News */}
+									<div className="absolute bottom-[8rem] md:bottom-[7rem] lg:bottom-[8rem] left-[2rem] flex gap-5 items-center z-30 text-xs lg:text-sm">
+										<p className="lg:text-sm text-orangeCS italic font-semibold">
+											RDK <span className="text-white">News</span>
+										</p>
+										<Image
+											src="/logo.png"
+											alt="Logo RDK"
+											width={16}
+											height={16}
+											className="z-30"
+										/>
+										<div className="lg:w-[3px] w-[1px] lg:h-6 h-6 bg-white"></div>
+										<p className="font-light italic text-white">
+											March, 12 2025
+										</p>
+									</div>
+
+									{/* Judul Artikel */}
+									<div className="absolute bottom-5 text-center text-sm md:text-xs lg:text-base font-semibold italic text-white p-4 lg:w-[80%] md:w-[90%] w-full left-1/2 transform -translate-x-1/2 z-30 font-body">
+										{article.title}
+									</div>
+
+									{/* Efek Smoke Green */}
+									<Image
+										src="/images/smoke-green.png"
+										alt="Logo RDK"
+										width={2000}
+										height={1000}
+										className="absolute bottom-0 left-0 right-0 z-20 h-[350px]"
+									/>
+								</div>
+							</Link>
+						</SwiperSlide>
+					))}
 				</Swiper>
+			) : (
+				<div className="flex space-x-4 py-6">
+					{Array.from({ length: 3 }).map((_, index) => (
+						<div
+							key={index}
+							className="bg-gray-200 animate-pulse w-[90%] lg:w-[25rem] h-[15rem] rounded-3xl"
+						></div>
+					))}
+				</div>
 			)}
 		</li>
 	);
